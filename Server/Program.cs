fdadc5c5bc19d66b8749144c57dcda71f00219d6
Eren ÖZTEN <priimalshade@gmail.com>
2022-06-30 00:23:45 +0300

    Create necessary members in and Main method in Server project

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatService
+{
+    class Program
+    {
+
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> _clienSockets = new List<Socket>();
+
+        // Receive buffer.  
+        private static byte[] _buffer = new byte[1024];
+
+        // will be used for getting time when connection made
+        private static DateTime? _requestTime;
+
+        // will be used for warn user if client sends more than one message in a second
+        private static int _warnCount = 0;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "SERVER SIDE";
+            Console.ReadKey();
+        }
+
+
+    }
+}
